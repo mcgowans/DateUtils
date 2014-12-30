8bd756f93963a0e36778825688990b6ce75cb1d1
blueark <bluearkdesign@gmail.com>
2014-12-30 22:59:23 +0000

    Added methods to calculate first and last days of a month.

---
+++ DateUtilsTests/DateUtilsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BlueArk;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DateUtilsTests
+{
+    [TestClass]
+    public class DateUtilsTests
+    {
+        [TestMethod]
+        public void Get_First_Day_Of_Month()
+        {
+            DateTime source1 = new DateTime(2014, 12, 15, 9, 10, 11);
+
+            DateTime expected1 = new DateTime(2014, 12, 1);
+
+            Assert.AreEqual(expected1, DateUtils.FirstDayOfMonth(source1));
+        }
+
+        [TestMethod]
+        public void Get_Last_Day_Of_Month()
+        {
+            DateTime source1 = new DateTime(2014, 12, 15, 9, 10, 11);
+
+            DateTime expected1 = new DateTime(2014, 12, 31);
+
+            Assert.AreEqual(expected1, DateUtils.LastDayOfMonth(source1));
+        }
+
+    }
+}
