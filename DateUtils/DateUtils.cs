1c3dd2e0e840cc1d0e24f3572bd59fdc120b5367
blueark <bluearkdesign@gmail.com>
2015-01-18 22:24:47 +0000

    Added Easter calculations Started adding some common holiday calculations. Started with Easter, as it's more of a challenge to calculate. The code was lifted from another project I did last year, but I can't recall where I got the algorithm from originally. Will add a credit if I find it.

--- DateUtils/DateUtils.cs
+++ DateUtils/DateUtils.cs
@@ -256,6 +256,82 @@ namespace BlueArk
         }
         #endregion
 
+        #region Common Holidays
+        #region Easter
+        /// <summary>
+        /// Calculate the date of Easter Sunday on a given year
+        /// </summary>
+        /// <param name="year">The year to calculate Easter Sunday from</param>
+        /// <returns>The Date of Easter Sunday on a given year.</returns>
+        public static DateTime EasterSunday(int year)
+        {
+            int day = 0;
+            int month = 0;
+
+            int g = year % 19;
+            int c = year / 100;
+            int h = (c - (int)(c / 4) - (int)((8 * c + 13) / 25) + 19 * g + 15) % 30;
+            int i = h - (int)(h / 28) * (1 - (int)(h / 28) * (int)(29 / (h + 1)) * (int)((21 - g) / 11));
+
+            day = i - ((year + (int)(year / 4) + i + 2 - c + (int)(c / 4)) % 7) + 28;
+            month = 3;
+
+            if (day > 31)
+            {
+                month++;
+                day -= 31;
+            }
+
+            return new DateTime(year, month, day).Date;
+        }
+
+        /// <summary>
+        /// Calculate the date of Easter Sunday this year.
+        /// </summary>
+        /// <returns>The date of Easter Sunday for the current year.</returns>
+        public static DateTime EasterSunday()
+        {
+            return EasterSunday(DateTime.Today.Year);
+        }
+
+        /// <summary>
+        /// Calculate the date of the next Easter Sunday
+        /// </summary>
+        /// <returns>Date of the next Easter Sunday from the current date.</returns>
+        public static DateTime NextEasterSunday()
+        {
+            DateTime thisEaster = EasterSunday();
+            if (DateTime.Today > thisEaster)
+            {
+                return EasterSunday(DateTime.Today.Year + 1);
+            }
+            else
+            {
+                return thisEaster;
+            }
+        }
+
+        /// <summary>
+        /// Calculate the date of the previous Easter Sunday
+        /// </summary>
+        /// <returns>Date of the previous Easter Sunday from the current date.</returns>
+        public static DateTime PreviousEasterSunday()
+        {
+            DateTime thisEaster = EasterSunday();
+            if (DateTime.Today < thisEaster)
+            {
+                return EasterSunday(DateTime.Today.Year - 1);
+            }
+            else
+            {
+                return thisEaster;
+            }
+        }
+
+        #endregion
+        #endregion
+
+
         #region
         // TODO... Lists of dates, like 'Every second Friday for the next year'.
         #endregion
